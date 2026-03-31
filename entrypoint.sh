62b88a1e2581498a87662a706af4d26c6e91b500
Rodrigo Pereira <rodrigo.sjhs@gmail.com>
2026-03-31 14:24:02 -0400

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,6 +42,10 @@ if [ -f "$CONFIG_FILE" ]; then
     echo "Configurando SSO Loglab..."
     occ config:system:set loglab_auth_login_url --value="https://infra.loglabprojetos.com.br/microsservices/auth/login" || true
 
+    echo "Configurações recomendadas..."
+    occ config:system:set default_phone_region --value="BR" || true
+    occ config:system:set maintenance_window_start --value="1" || true
+
     echo "Ativando tema Loglab..."
     occ app:enable loglab_theme || true
 
