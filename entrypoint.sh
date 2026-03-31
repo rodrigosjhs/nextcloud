91a5bec1a5b665ea67e56f85251320568e2f7a43
Rodrigo Pereira <rodrigo.sjhs@gmail.com>
2026-03-31 13:47:56 -0400

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 apache2-foreground &
 
 echo "Aguardando Nextcloud iniciar..."
-sleep 20  # aguarda o container iniciar (pode aumentar se necessário)
+sleep 20
 
 CONFIG_FILE="/var/www/html/config/config.php"
 
@@ -13,6 +13,10 @@ if [ -f "$CONFIG_FILE" ]; then
     echo "Configurando trusted domains..."
     sudo -u www-data php /var/www/html/occ config:system:set trusted_domains 0 --value="localhost" || true
     sudo -u www-data php /var/www/html/occ config:system:set trusted_domains 1 --value="intranet.loglabprojetos.com.br" || true
+
+    echo "Configurando overwrite protocol e host..."
+    sudo -u www-data php /var/www/html/occ config:system:set overwriteprotocol --value="https" || true
+    sudo -u www-data php /var/www/html/occ config:system:set overwritehost --value="intranet.loglabprojetos.com.br" || true
 
     echo "Configurando URL de login Loglab..."
     sudo -u www-data php /var/www/html/occ config:system:set loglab_auth_login_url --value="https://infra.loglabprojetos.com.br/microsservices/auth/login" || true
