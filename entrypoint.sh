2d2a6b5eb7fdd113d4f1edd2ec21b553bceb6815
Rodrigo Pereira <rodrigo.sjhs@gmail.com>
2026-03-31 14:07:09 -0400

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,14 @@ if [ -f "$CONFIG_FILE" ]; then
     echo "Configurando overwrite protocol e host..."
     sudo -u www-data php /var/www/html/occ config:system:set overwriteprotocol --value="https" || true
     sudo -u www-data php /var/www/html/occ config:system:set overwritehost --value="intranet.loglabprojetos.com.br" || true
+    sudo -u www-data php /var/www/html/occ config:system:set overwrite.cli.url --value="https://intranet.loglabprojetos.com.br" || true
+
+    echo "Configurando trusted proxies..."
+    sudo -u www-data php /var/www/html/occ config:system:set trusted_proxies 0 --value="10.10.100.35" || true
+    sudo -u www-data php /var/www/html/occ config:system:set trusted_proxies 1 --value="10.10.100.30" || true
+    sudo -u www-data php /var/www/html/occ config:system:set trusted_proxies 2 --value="10.0.0.0/8" || true
+    sudo -u www-data php /var/www/html/occ config:system:set trusted_proxies 3 --value="172.16.0.0/12" || true
+    sudo -u www-data php /var/www/html/occ config:system:set trusted_proxies 4 --value="192.168.0.0/16" || true
 
     echo "Configurando URL de login Loglab..."
     sudo -u www-data php /var/www/html/occ config:system:set loglab_auth_login_url --value="https://infra.loglabprojetos.com.br/microsservices/auth/login" || true
